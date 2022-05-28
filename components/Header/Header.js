a2d85150ee8973bb4f57cc0d981fe5c338e9facf
unknown <mayankjoshi996007@gmail.com>
2022-05-28 10:38:07 +0530

    update

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -286,7 +286,7 @@ console.log(e)
   <div className="container-fluid">
     <a className="navbar-brand" href="#">
       
-    <Image  className='logo-dark' id="logo_header" src="/logo_dark.png" srcSet="/logo_dark2x.png" alt="nft-gaming" width="133" height="56"  />
+    <Image  className='logo-dark' id="logo_header" src="/logo.png" srcSet="/logo.png" alt="nft-gaming" width="80" height="56"  /><h4>Exchange</h4>
        </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
